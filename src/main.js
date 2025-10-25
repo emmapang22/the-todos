3b534df13fa0b884f10e337d15e3597e7484036d
Emma Pang <emmapang3@gmail.com>
2025-10-25 23:00:06 +0200

    Save user input from the form to localStorage using Task object

--- src/main.js
+++ src/main.js
@@ -1,1 +1,24 @@
+import { Task } from "./models/Task";
 import "./style.css";
+
+let tasks = [];
+
+const handleSubmit = (e) => {
+  e.preventDefault();
+
+  const title = document.getElementById("title").value;
+  const deadline = document.getElementById("deadline").value;
+  const priority = document.getElementById("select-priority").value;
+
+  const newTask = new Task(title, deadline, priority);
+
+  tasks.push(newTask);
+
+  localStorage.setItem("task", JSON.stringify(tasks));
+};
+
+const form = document.getElementById("form");
+
+if (form) {
+  form.addEventListener("submit", handleSubmit);
+}
---
+++ src/models/Task.js
@@ -0,0 +1,11 @@
+export class Task {
+  title;
+  deadline;
+  priority;
+
+  constructor(title, deadline, priority) {
+    this.title = title;
+    this.deadline = deadline;
+    this.priority = priority;
+  }
+}
